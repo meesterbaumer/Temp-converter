57fe7f5490f29a1cb118478c8457d416e6138dc4
Michael Baumer <meesterbaumer@mac.com>
2019-09-23 22:27:36 -0400

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('test');
+const fahrenheitSelected = document.getElementById("fahrenheit")
+const celsiusSelected = document.getElementById("celsius")
+
+
+const printToDom = (DivID,textToPrint) => {
+    document.getElementById(DivID).innerHTML = textToPrint
+}
+
+printToDom("output", 'hello');
+
+    
+    
+    
+
+
+const formulaChooser = () => {
+    
+}
+
