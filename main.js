68a7caa1f15d5300442cbbe1c98bc83125a37f8f
Michael Baumer <meesterbaumer@mac.com>
2019-10-02 17:57:12 -0500

    Got temp displaying in output

--- main.js
+++ main.js
@@ -9,11 +9,19 @@ const printToDom = (DivID,textToPrint) => {
 printToDom("output", 'hello');
 
     
+const toFahrenheit = (cTemp) => {
+    let fResult = Math.round((cTemp * 1.8) + 32);
+    printToDom("output", fResult) 
+}
     
-    
-
+const toCelsius = (fTemp) => {
+    let cResult = Math.round((fTemp - 32) / 1.8);
+    printToDom("output", cResult);  
+}
 
 const formulaChooser = () => {
     
 }
 
+toFahrenheit(0)
+toCelsius(76.565)
